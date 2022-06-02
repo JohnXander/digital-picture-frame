d83d4d1253a19000a0857f74a4b538723bcf88cc
John Xander <johnblox5@gmail.com>
2022-06-02 11:10:33 +0100

    JSON data increased

--- script.js
+++ script.js
@@ -3,20 +3,18 @@ const imgPicture = document.getElementById("image")
 const imgDate = document.getElementById("date")
 const btn = document.getElementById("btn")
 
-
-
-fetch("./pictures.json")
+const getPictures = () => {
+    fetch("./pictures.json")
     .then(resp => resp.json())
-    .then(data => getPictures(data))
-
-const getPictures = (data) => {
-    const randomIdx = Math.floor(Math.random() * data.pictures.length)
-    imgLocation.innerHTML = data.pictures[randomIdx].location
-    imgPicture.src = data.pictures[randomIdx].picture
-    imgPicture.alt = data.pictures[randomIdx].location
-    imgDate.innerHTML = data.pictures[randomIdx].date
+        .then(data => {
+            const randomIdx = Math.floor(Math.random() * data.pictures.length)
+            imgLocation.innerHTML = data.pictures[randomIdx].location
+            imgPicture.src = data.pictures[randomIdx].picture
+            imgPicture.alt = data.pictures[randomIdx].location
+            imgDate.innerHTML = data.pictures[randomIdx].date
+        })
 }
 
-console.log(getPictures())
+// setInterval(getPictures, 3000)
 
 
